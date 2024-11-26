814c320593c9e6c1112a4045a15aba5513ce346f
Beat <beat.erne@hotmail.com>
2024-11-26 11:18:51 +0100

    circleshape.py and FPS limit

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import pygame
 from pygame import display
+from pygame.examples.sprite_texture import clock
 
 from constants import *
 
 def main():
-
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init()
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.time.Clock
+    dt = 0
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-        screen.fill("black")
-        display.flip()
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+
+            screen.fill("black")
+            display.flip()
+
+            # limite the framerate to 60 FPS
+            dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
